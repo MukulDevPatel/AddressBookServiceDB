b0c55ab9d50c658176141d9c46578bf9a11f8fb7
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-19 21:05:30 +0530

    [Mukul Dev] Add: We create a database in MSSQL by using sql client

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
-﻿namespace AddressBookServiceDB;
+﻿using System.Data.SqlClient;
+
+namespace AddressBookServiceDB;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Address Book Service DataBase");
+
+        string connectionString = @"Data Source = DESKTOP-53EDIV8; Initial Catalog=master; Integrated Security = True";
+
+        string databaseName = "AddressBookDB";
+        try
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            string createDatabaseQuery = $"create database {databaseName}";
+
+            SqlCommand command = new SqlCommand(createDatabaseQuery, connection);
+            command.ExecuteNonQuery();
+            Console.WriteLine($"Databse {databaseName} created");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error"+ ex.Message);
+        }
+        Console.ReadLine();
     }
 }
